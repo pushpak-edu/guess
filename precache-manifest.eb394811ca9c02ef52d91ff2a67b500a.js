e80c5e02e951860fddacad13e9a1ce9e12aeb4c2
Pushpak Sharma <pushpaksharma@Pushpaks-Air.ht.home>
2023-02-09 10:17:44 +0530

    Updates

---
+++ precache-manifest.eb394811ca9c02ef52d91ff2a67b500a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcba531c5860f8baf2e34455303510db",
+    "url": "/guess/index.html"
+  },
+  {
+    "revision": "f2825ff44688da527368",
+    "url": "/guess/static/css/main.5b39239b.chunk.css"
+  },
+  {
+    "revision": "b57c39673f650a7a3894",
+    "url": "/guess/static/js/2.6f4f9af9.chunk.js"
+  },
+  {
+    "revision": "f2825ff44688da527368",
+    "url": "/guess/static/js/main.c5db2679.chunk.js"
+  },
+  {
+    "revision": "67106eac33cf54ddaa0c",
+    "url": "/guess/static/js/runtime~main.963def73.js"
+  }
+]);
